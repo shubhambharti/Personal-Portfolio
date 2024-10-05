299d48f1872347ab1d7a970f2d38820f5c02c311
shubhambharti <shubhambharti711@.com>
2024-10-05 16:32:10 +0530

    resumeupdate

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var sidmenu = document.getElementById("sidmenu");
+        function openmenu() {
+            sidmenu.style.right = "0";
+        }
+        function closemenu() {
+            sidmenu.style.right = "-200px";
+        }
+
